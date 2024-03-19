a685da4f7047d254bcfbec030ddb2715e22ac79c
marianaguiomar <mmarianaguiomar@gmail.com>
2024-03-19 12:17:45 +0000

    TODOs e tal

--- src/main/java/Barrel.java
+++ src/main/java/Barrel.java
@@ -1,8 +1,12 @@
 import java.net.*;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Barrel extends Thread {
-    //parte do multicast
+    //
+    // parte do multicast
+    //
     private String MULTICAST_ADDRESS = "224.3.2.1";
     private int PORT = 4321;
 
@@ -23,7 +27,10 @@ public class Barrel extends Thread {
 
                 byte[] buffer = new byte[256];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                // vai receber um URL e as palavras associadas (+ título e citação de texto?)
                 socket.receive(packet);
+
+                //TODO -> colocar resultados na classe e no índice remissivo
 
                 System.out.println("Received packet from " + packet.getAddress().getHostAddress() + ":"
                         + packet.getPort() + " with message:");
@@ -37,7 +44,23 @@ public class Barrel extends Thread {
         }
     }
 
+    //
+    // classe URL
+    //
 
+
+
+    //
+    // parte do índice remissivo
+    //
+
+    // String -> palavra, HashSet<String> -> URLs a que se relaciona
+    HashMap<String, HashSet<String>> index;
+
+
+    //
+    // main
+    //
 
     public static void main(String[] args) {
         Barrel barrel = new Barrel();
--- src/main/java/Downloader.java
+++ src/main/java/Downloader.java
@@ -11,15 +11,14 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Downloader extends Thread {
-    //TODO -> ESTAR SEMPRE A RECEBER URL DA QUEUE
-    //TODO -> ver o que é mais indicado usar. queue? deque? por enquanto fica linked list (que é uma forma de queue?)
+    //TODO -> ficou concurrent double deque
     //TODO -> ver se deixa a queue aqui, num ficheiro à parte, dentro de outras cenas, etc
 
     //Multicast section
     //TODO -> estes são os valores da ficha, verificar se são os corretos
     private String MULTICAST_ADDRESS = "224.3.2.1";
     private int PORT = 4321;
-    private long SLEEP_TIME = 5000;
+    //private long SLEEP_TIME = 5000;
 
     // TODO -> nome da thread, mudar potencialmente para Downloader <número do downloader>
 
@@ -27,7 +26,7 @@ public class Downloader extends Thread {
 
 
     public Downloader() {
-        //TODO -> nome (por um argumento no main? para distinguir os downloaders)
+        //TODO -> nome (por um argumento no main? para distinguir os downloaders) por id downloader
         super("Downloader " + (long) (Math.random() * 1000));
         this.channel = channel;
     }
@@ -39,17 +38,20 @@ public class Downloader extends Thread {
         try {
             socket = new MulticastSocket();  // create socket without binding it (only for sending)
             while (true) {
-                //TODO -> esta parte será o que enviará as mensagens
+                //TODO -> esta parte será o que enviará as mensagens (de momento só tem o código da ficha 3)
                 String message = this.getName() + " packet " + counter++;
                 byte[] buffer = message.getBytes();
 
                 InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
+
+                // vai enviar o current link a ser analisado e os tokens associados. os links encontrados são postos na fila para serem analisados
+                // TODO -> ver se se dá para enviar objetos por multicast, se sim por aqui a classe URL
                 socket.send(packet);
 
                 try {
                     //TODO -> ver do sleep, ver se é necessário (não é, era só para o exercício)
-                    sleep((long) (Math.random() * SLEEP_TIME));
+                    sleep((long) (Math.random()));
                 } catch (InterruptedException e) { }
             }
         } catch (IOException e) {
@@ -64,7 +66,9 @@ public class Downloader extends Thread {
 
     LinkedList<String> URLQueue = new LinkedList<>();
 
+    // TODO -> ordem de adicionar os links
     public void addToQueue() {
+        // TODO -> vai começar já com sites? (por enquanto é melhor deixar um i guess)
         URLQueue.add("https://sapo.pt");
         //URLQueue.add("https://observador.pt");
         //URLQueue.add("https://inforestudante.uc.pt");
@@ -77,11 +81,14 @@ public class Downloader extends Thread {
         Downloader downloader = new Downloader();
         downloader.start();
 
+        // TODO -> verificar isto
         downloader.URLQueue.clear();
 
         downloader.addToQueue();
 
         //String url = args[0];
+
+        //TODO -> ESTAR SEMPRE A RECEBER URL DA QUEUE (while queue isn't empty)
 
         for (String url2 : downloader.URLQueue) {
             try {
@@ -91,10 +98,13 @@ public class Downloader extends Thread {
                 int countTokens = 0;
 
                 // exemplo -> este imprime os primeiros 100 tokens em lowercase
+                // TODO -> acho que têm de ser todos os tokens que existem?
+                // TODO -> limitar os tokens para não conterem palavras reservadas
                 while (tokens.hasMoreElements() && countTokens++ < 100)
                     System.out.println(tokens.nextToken().toLowerCase());
 
                 // seleciona todos os elementos <a> que têm um atributo href -> tipicamente são hiperlinks
+                // TODO -> estes hiperlinks não serão enviados por Multicast, vão ser inseridos na URLQueue
                 Elements links = doc.select("a[href]");
                 for (Element link : links)
                     // imprime o texto (o que está visível e clicável) e o url absoluto de cada link
