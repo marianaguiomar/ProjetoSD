5201540f7dc66832ad290686ca5f357245c8098a
marianaguiomar <mmarianaguiomar@gmail.com>
2024-03-21 17:19:12 +0000

    início de filtrar stopwords

--- src/main/java/Downloader.java
+++ src/main/java/Downloader.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 //TODO -> EXCEPTION MALFORMED URL (JSOUP) PARA URLS INVÁLIDOS
@@ -19,6 +20,13 @@ public class Downloader {
     //private long SLEEP_TIME = 5000;
     private String channel;
     QueueInterface queue;
+
+    //TODO -> mais stopwords
+    String[] stopwords = {"a", "an", "and", "are", "as", "at", "be", "but", "by",
+            "for", "if", "in", "into", "is", "it",
+            "no", "not", "of", "on", "or", "such",
+            "that", "the", "their", "then", "there", "these",
+            "they", "this", "to", "was", "will", "with"};
 
     public Downloader(String queuePath) throws MalformedURLException, RemoteException, NotBoundException {
         this.queue = (QueueInterface) Naming.lookup(queuePath);
@@ -39,13 +47,19 @@ public class Downloader {
                 String url2 = this.queue.fetchURL();
                 Document doc = Jsoup.connect(url2).get();
                 StringTokenizer tokens = new StringTokenizer(doc.text());
-                int countTokens = 0;
 
                 // exemplo -> este imprime os primeiros 100 tokens em lowercase
                 // TODO -> acho que têm de ser todos os tokens que existem?
                 // TODO -> limitar os tokens para não conterem palavras reservadas
-                //while (tokens.hasMoreElements() && countTokens++ < 100)
-                    //System.out.println(tokens.nextToken().toLowerCase());
+                while (tokens.hasMoreElements()) {
+                    boolean isStopword;
+                    if (Arrays.asList(stopwords).contains(tokens.nextToken())) isStopword = true;
+                    else isStopword = false;
+                    if (!isStopword) {
+                        //System.out.println(tokens.nextToken().toLowerCase());
+                    }
+
+                }
 
                 // seleciona todos os elementos <a> que têm um atributo href -> tipicamente são hiperlinks
                 // TODO -> estes hiperlinks não serão enviados por Multicast, vão ser inseridos na URLQueue
