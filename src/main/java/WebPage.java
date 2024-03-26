4a1498606e30571b19b8e7971ac827df56cfb508
SauloJoseMendes <invalidot@gmail.com>
2024-03-26 14:44:59 +0000

    WebPage.java hotfix

--- src/main/java/WebPage.java
+++ src/main/java/WebPage.java
@@ -1,6 +1,6 @@
 public record WebPage(String hiperlink, String titulo, String citacao) {
     @Override
     public String toString() {
-        return String.format("TITLE: %s\n\tURL: %s\n\tCITATION: %s}",titulo,hiperlink, citacao);
+        return String.format("TITLE: %s\n\tURL: %s\n\tCITATION: %s",titulo,hiperlink, citacao);
     }
 }
