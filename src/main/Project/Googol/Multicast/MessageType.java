d59204d44a25acb02f625d309ad53fda02774e37
SauloJoseMendes <invalidot@gmail.com>
2024-04-06 18:20:46 +0100

    help

--- src/main/Project/Googol/Multicast/MessageType.java
+++
@@ -1,7 +0,0 @@
-package Googol.Multicast;
-/**
- * Enum representing different types of multicast messages.
- */
-public enum MessageType {
-    TITLE, CITATION, TOKENS, CONNECTIONS, CONFIRMATION
-}
